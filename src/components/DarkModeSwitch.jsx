39d780b7855b32ea2278e2ad671d3533bc1afeb5
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-17 13:34:55 -0500

    updated the DarkModeSwitch component to prevent errors during page refresh

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -2,14 +2,20 @@
 import { MdLightMode } from 'react-icons/md';
 import { BsFillMoonFill} from 'react-icons/bs';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 function DarkModeSwitch() {
 
  const { systemTheme, theme, setTheme } = useTheme();
-    const currentTheme = theme === 'system' ? systemTheme : theme
+ const [mounted, setMounted] = useState(false);
+ 
+ useEffect(() => setMounted(true),
+ []);
+
+ const currentTheme = theme === 'system' ? systemTheme : theme
   return (
     <>
-        { currentTheme === 'dark' ? (
+        { mounted && currentTheme === 'dark' ? (
         <MdLightMode className='text-xl cursor-pointer hover:text-amber-300' onClick={() => setTheme('light')}/>
         ) :  <BsFillMoonFill className='text-xl cursor-pointer hover:text-amber-300' onClick={() => setTheme('dark')}/> }
     </>
