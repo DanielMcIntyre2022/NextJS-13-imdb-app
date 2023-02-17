fd8eb26d19a3bbf003ea79b600f04d75d8b63acd
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-17 13:58:58 -0500

    added about folder to create about route and then created page component within that folder to display about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,16 @@
+
+function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+        <h1 className="text-2xl font-medium text-amber-300">About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis pariatur sed, quos nostrum facere omnis tenetur inventore asperiores error deserunt dicta sunt nihil consectetur! 
+            Autem, facilis? Nemo, repudiandae blanditiis? Ullam.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, quibusdam omnis asperiores inventore sequi ratione. Accusamus consectetur, error numquam fugiat perspiciatis magnam. Sunt, enim nulla. 
+            Autem repudiandae ut cupiditate!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente minus voluptates harum quis dicta placeat aut aperiam pariatur quasi eveniet, iure molestias quae ut aliquam esse repudiandae delectus 
+            dignissimos amet?</p>
+    </div>
+  )
+}
+
+export default page
