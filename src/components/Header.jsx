4c0b7e5e1b08509acaef63d294af397b0fe03151
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-17 11:34:09 -0500

    added secondary menu item within Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import MenuItem from "./MenuItem";
 import {AiFillHome} from 'react-icons/ai';
+import {BsFillInfoCircleFill} from 'react-icons/bs';
 
 function Header() {
   return (
     <div>
-        <div className="">
-            <MenuItem title="HOME" address={'/'} Icon={AiFillHome}/>
+        <div className="flex">
+            <MenuItem title="HOME" address={'/'} Icon={AiFillHome}/>   
+            <MenuItem title="ABOUT" address={'/about'} Icon={BsFillInfoCircleFill}/>
         </div>
 
         <div className="">
