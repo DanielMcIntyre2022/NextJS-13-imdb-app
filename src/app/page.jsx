2d6cfe4f70a65d472b8bf27ec444dc647d9f1b2b
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-17 15:19:59 -0500

    attempted to fetch data from TMDB api within home page component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,24 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
+const API_KEY = process.env.API_KEY;
 
-const inter = Inter({ subsets: ['latin'] })
+export default async function Home({ searchParams }) {
 
-export default function Home() {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(`https://api.themoviedb.org/3/${
+    genre === "fetchTopRated" ? "movie/top_rated" : 
+  "trending/all/week"} ?api_key=${API_KEY}&language=en-US&page-1`, {
+    next: { revalidate: 10000 }
+  });
+
+  const data = await res.json();
+
+  const results = data.results;
+
+  console.log(results);
+
   return (
-   <div><h1 className='text-red-500'>Home</h1></div>
+   <div>
+        <h1 className='text-red-500'>Home</h1>
+    </div>
   )
-}
+};
