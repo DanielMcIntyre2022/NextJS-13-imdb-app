9b0af9e2c824b50138957c9acb9256a75e39ce2c
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-17 11:24:03 -0500

    creation of MenuItem component and props passed down from Header component to MenuItem component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import MenuItem from "./MenuItem";
+import {AiFillHome} from 'react-icons/ai';
 
 function Header() {
   return (
     <div>
         <div className="">
-
+            <MenuItem title="HOME" address={'/'} Icon={AiFillHome}/>
         </div>
 
         <div className="">
