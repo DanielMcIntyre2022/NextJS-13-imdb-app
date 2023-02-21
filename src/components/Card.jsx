302c2006d587c25543b91998eb22280e6671e03b
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-21 16:41:41 -0500

    successfully fetched and displayed image data from api

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
 
 function Card({result}) {
   return (
     <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 
     sm:shadow-md rounded-lg sm:border sm:border-slate-400
     sm:m-2 transition-shadow duration-200">
-        {result.id}
+        <Link href={`/movie/${result.id}`}>
+            <Image src={
+                `https://image.tmdb.org/t/p/original/${result.backdrop_path 
+                || result.poster_path}`
+                }
+                width={500}
+                height={300}
+                >
+            </Image>
+        </Link>
     </div>
   )
-}
+};
 
 export default Card;
