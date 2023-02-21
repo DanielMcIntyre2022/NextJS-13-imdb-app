59e7cbc93a961099111e1d5a9d637ccfb4c421bc
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-21 16:28:35 -0500

    styled the Results and Card components for various screen sizes

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,7 +2,8 @@ import Card from './Card';
 
 function Results({results}) {
   return (
-    <div>
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 
+    xl:grid-cols-4 2x1:grid-cols-5 max-w-6x1 mx-auto py-4'>
         {results.map((result) => (
           <Card key={result.id} result={result}/>
         ))}
