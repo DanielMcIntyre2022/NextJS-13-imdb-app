2ec0ee75abd7b4d88ced7c8e582de34fe62408c6
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-22 17:00:52 -0500

    added useState to form inputs to track and store user input in state and create handleSubmit function using useRouter to push user to url with search term

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -1,9 +1,27 @@
+'use client'
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 function Searchbox() {
+
+const [search, setSearch] = useState("");
+
+const router = useRouter();
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!search) return;
+    router.push(`/search/${search}`);
+};
+
   return (
-    <form className="flex max-w-6xl mx-auto 
+    <form 
+    onSubmit={handleSubmit}
+    className="flex max-w-6xl mx-auto 
     justify-between items-center px-5">
         <input type="text" 
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         placeholder="Search for movie..."
         className="w-full h-14 rounded-sm placeholder-gray-500 
         outline-none bg-transparent flex-1"
