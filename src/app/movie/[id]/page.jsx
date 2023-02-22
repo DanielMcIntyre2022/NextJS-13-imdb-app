e6f634336d7a89dee1ef2117f975fdf4783ea628
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-22 15:44:09 -0500

    added Image component to movie page with styles from Cart component

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 async function getMovie(movieId) {
         const res = await fetch(`https://api.themoviedb.org/3/movie/
         ${movieId}?api_key=${process.env.API_KEY}`);
@@ -9,8 +11,28 @@ async function MoviePage({params}) {
     const movie = await getMovie(movieId);
   
     return (
-    <div>
-        <h1>{movie.title || movie.name}</h1>
+    <div className="w-full">
+        <div className="p-4 md:pt-8 flex flex-col md:flex-row 
+        items-center content-center max-w-6xl mx-auto md:space-x-6">
+            <Image src={
+                `https://image.tmdb.org/t/p/original/${movie.backdrop_path 
+                || movie.poster_path}`
+                }
+                width={500}
+                height={300}
+                className="rounded-lg"
+                placeholder="blur"
+                blurDataURL="/spinner.svg"
+                alt="Movie poster"
+                style={
+                  {
+                    maxWidth: "100%",
+                    height: "100%"
+                  }
+                }
+                >
+            </Image>
+        </div>
     </div>
   )
 };
