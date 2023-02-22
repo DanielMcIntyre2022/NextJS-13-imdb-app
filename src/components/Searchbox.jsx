f1455a83a62381b3547ddee3ec5d91204cd3c036
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-22 16:47:16 -0500

    styled Searchbox component form and input

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
 
 function Searchbox() {
   return (
-    <div>Searchbox</div>
+    <form className="flex max-w-6xl mx-auto 
+    justify-between items-center px-5">
+        <input type="text" 
+        placeholder="Search for movie..."
+        className="w-full h-14 rounded-sm placeholder-gray-500 
+        outline-none bg-transparent flex-1"
+        />
+        <button 
+        className="text-amber-300
+        disabled:text-gray-400"
+        type="submit">Search</button>
+    </form>
   )
-}
+};
 
 export default Searchbox;
